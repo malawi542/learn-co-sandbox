43a061e54b8b06f62a169ab6e8a50cd33662add5
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-08-21 03:41:11 +0000

    Automatically backed up by Learn

--- exercise.rb
+++ exercise.rb
@@ -14,4 +14,4 @@ def greeting_programmer(name, language)
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
 
-greeting_programmer(David, Arabic)
+greeting_programmer("David", "Arabic")
